9be28ef0a689719370e86a8891e92dda2217aee0
Soshi <soshi.mimicry@gmail.com>
2020-04-03 14:51:40 +0200

    working fine

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace AB4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Geben Sie ein Nummer grosser als null ein!");
+
+            int eingabe = Convert.ToInt32(Console.ReadLine());
+            int i = eingabe;
+            int summe = eingabe;
+            /* Null Ausnahme */
+            if (eingabe == 0)
+            {
+                summe = 1;
+                Console.WriteLine("Das Ergebnis ist: {0}", summe);
+            }
+            else if (eingabe > 0)
+            {
+                while (i >= 2)
+                {
+                    int things = i - 1;
+                    summe = summe * things;
+                    i = i - 1;
+                }
+                Console.WriteLine("Das Ergebnis ist: {0}", summe);
+            }
+            else
+            {
+                Console.WriteLine("Nicht Definiert!");
+            }
+
+        }
+    }
+}
